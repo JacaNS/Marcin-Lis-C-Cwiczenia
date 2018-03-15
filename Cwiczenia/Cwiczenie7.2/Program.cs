8fbea5c69d4c634d1782bb54e9897017bf6a80d3
JacaNS <komurkiewiczjacek@gmail.com>
2018-03-15 23:06:25 +0100

    Cwiczenie 7.2

---
+++ Cwiczenia/Cwiczenie7.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 256;
+            Console.WriteLine(a >> 2);
+            Console.WriteLine(a << 2);
+            Console.ReadKey();
+        }
+    }
+}
