d4ea7def7fd3b8e2af532c0bc2828c74ed4beeb6
JacaNS <komurkiewiczjacek@gmail.com>
2018-03-16 00:02:29 +0100

    Cwiczenie 7.6

---
+++ Cwiczenia/Cwiczenie 7.6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_7._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            Console.WriteLine(a == b - 10);
+            Console.WriteLine(b == a + 10);
+            Console.ReadKey();
+        }
+    }
+}
