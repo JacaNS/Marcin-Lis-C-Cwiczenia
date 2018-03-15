4324cbd04cd3aaf7af9753bb39f1ac9ec121d6ec
JacaNS <komurkiewiczjacek@gmail.com>
2018-03-15 23:57:23 +0100

    Cwiczenie 7.5

---
+++ Cwiczenia/Cwiczenie 7.5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_7._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 1;
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(++a);
+            Console.WriteLine(b *= 4);
+            Console.ReadKey();
+        }
+    }
+}
