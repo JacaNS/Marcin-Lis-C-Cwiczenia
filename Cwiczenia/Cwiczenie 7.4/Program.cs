56467060c543ffa96785506aa4d79908d73cfd30
JacaNS <komurkiewiczjacek@gmail.com>
2018-03-15 23:41:52 +0100

    Cwiczenie 7.4

---
+++ Cwiczenia/Cwiczenie 7.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_7._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 2;
+            Console.WriteLine(a ^= b);
+            Console.WriteLine(a ^= b);
+            Console.ReadKey();
+        }
+    }
+}
