754273f7e7380134c78fdf064dbf53d2ee1299cb
JacaNS <komurkiewiczjacek@gmail.com>
2018-03-15 22:56:48 +0100

    Cwiczenie7.1

--- Cwiczenia/Cwiczenie7.1/Program.cs
+++ Cwiczenia/Cwiczenie7.1/Program.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cwiczenia
+namespace Cwiczenie7._1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int a = 10;
-            a += 10;
-            Console.Write(a);
+            int a = 100, b = 3, c = 2, wynik;
+            wynik = a % b % c;
+            Console.WriteLine(wynik);
             Console.ReadKey();
         }
     }
