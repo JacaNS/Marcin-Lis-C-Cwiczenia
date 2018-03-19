7d272918b4e13f2f67ca2be52165e0f4d7d8d554
JacaNS <komurkiewiczjacek@gmail.com>
2018-03-19 12:29:09 +0100

    Cwiczenie 8.1

---
+++ Cwiczenia/Cwiczenie 8.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20, b = 10;
+
+            if(a % b == 0)
+            {
+                Console.WriteLine("Wynikiem dzielenia modulo: a przez b jest 0");
+            }
+            Console.ReadKey();
+        }
+    }
+}
