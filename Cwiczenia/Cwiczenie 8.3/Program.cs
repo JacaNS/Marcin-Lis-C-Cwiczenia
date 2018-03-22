81e07dc41cfac052ca1212a1c3c1d0f10f54ef3e
JacaNS <komurkiewiczjacek@gmail.com>
2018-03-22 10:15:33 +0100

    Cwiczenie 8.3

---
+++ Cwiczenia/Cwiczenie 8.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_8._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+
+            if(a > 0)
+            {
+                Console.WriteLine("Wartosc bezwzględna wynosi " + a);
+            }
+
+            else if(a < 0)
+            {
+                Console.WriteLine("Wartosc bezwzględna wynosi... " + -a);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
