b8810fdca9dafe0b987a9e31f9499e89bf5fb233
JacaNS <komurkiewiczjacek@gmail.com>
2018-03-25 20:34:25 +0200

    Cwiczenie 9.2

---
+++ Cwiczenia/Cwiczenie 9.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_9._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            Console.WriteLine("Podaj liczbę A");
+            int liczba1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj liczbę B");
+            int liczba2 = Convert.ToInt32(Console.ReadLine());
+            a = liczba1 % liczba2 == 0 ? true : false;
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
