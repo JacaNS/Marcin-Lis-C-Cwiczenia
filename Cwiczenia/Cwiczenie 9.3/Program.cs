0eab97313ac7c0d8ce53b72ba571239d91234426
JacaNS <komurkiewiczjacek@gmail.com>
2018-03-25 21:05:06 +0200

    Cwiczenie 9.3

---
+++ Cwiczenia/Cwiczenie 9.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_9._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba = Convert.ToInt32(Console.ReadLine());
+
+            liczba = liczba >= 0 ? liczba : -liczba;
+
+            Console.WriteLine(liczba);
+            Console.ReadKey();
+        }
+    }
+}
