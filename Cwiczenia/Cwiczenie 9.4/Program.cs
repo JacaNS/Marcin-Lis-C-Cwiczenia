e7d9c5e7eac2051943ed9e8e4f643dbce15fdbcf
JacaNS <komurkiewiczjacek@gmail.com>
2018-03-26 21:11:53 +0200

    Cwiczenie 9.4

---
+++ Cwiczenia/Cwiczenie 9.4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_9._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Liczba A ma wartość: 1");
+                    break;
+                case 4:
+                    Console.WriteLine("Liczba A ma wartość: 4");
+                    break;
+                case 8:
+                    Console.WriteLine("Liczba A ma wartość: 8");
+                    break;
+                case 2:
+                    Console.WriteLine("Liczba A ma wartość: 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Liczba A ma wartość: 3");
+                    break;
+                case 7:
+                    Console.WriteLine("Liczba A ma wartość: 7");
+                    break;
+                default:
+                    Console.WriteLine("Zadna z wymienionych");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
