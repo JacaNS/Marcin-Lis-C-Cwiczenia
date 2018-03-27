d491806a39ab60fa8aba9239287b8a654b9e1795
JacaNS <komurkiewiczjacek@gmail.com>
2018-03-27 21:27:36 +0200

    Cwiczenie 10.1

---
+++ Cwiczenia/Cwiczenie 10.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Liczby podzielne przez 2 to " + i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
