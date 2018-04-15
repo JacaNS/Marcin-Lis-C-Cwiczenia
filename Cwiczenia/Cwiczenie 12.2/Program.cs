d48950ee42d1673a14f28afe56bcd9216444a27e
JacaNS <komurkiewiczjacek@gmail.com>
2018-04-15 20:44:37 +0200

    Cwiczenie 12.2

---
+++ Cwiczenia/Cwiczenie 12.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_12._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tablica = new int[10];
+            for(int i = 0; i < tablica.Length; i++)
+            {
+                tablica[i] = 10 + (i * 10);
+                Console.WriteLine(tablica[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
