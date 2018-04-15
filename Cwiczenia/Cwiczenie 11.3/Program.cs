e16f3a20065e0e0f17521839c2fe9f4c0840ee74
JacaNS <komurkiewiczjacek@gmail.com>
2018-04-15 18:14:52 +0200

    Cwiczenie 11.3

---
+++ Cwiczenia/Cwiczenie 11.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_11._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 100;
+            while(i-- >= 1)
+            {
+                if (i % 3 != 0 & i % 4 != 0)
+                    Console.WriteLine(i);
+            }
+
+            int j = 100;
+            while (j-- >= 1)
+            {
+                if (j % 3 == 0)
+                    continue;
+                if (j % 4 == 0)
+                    continue;
+                Console.WriteLine(j);
+            }
+            Console.ReadKey();
+        }
+    }
+}
