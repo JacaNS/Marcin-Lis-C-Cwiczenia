6ea238eed512145b95e1cc204a0a3c1c946af2a9
JacaNS <komurkiewiczjacek@gmail.com>
2018-04-15 18:55:51 +0200

    Cwiczenie 11.5

---
+++ Cwiczenia/Cwiczenie 11.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_11._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 2 == 0)
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
