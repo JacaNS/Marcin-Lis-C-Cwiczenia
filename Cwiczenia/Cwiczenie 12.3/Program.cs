d4e1dd7d5f7c7e779dcca31b9f15f80ff9994345
JacaNS <komurkiewiczjacek@gmail.com>
2018-04-15 22:56:58 +0200

    Cwiczenie 12.3

---
+++ Cwiczenia/Cwiczenie 12.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_12._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] tablica = new bool[20];
+            for(int i = 0; i < tablica.Length; i++)
+            {
+                if (i % 2 == 0)
+                    tablica[i] = true;
+                if (i % 2 != 0)
+                    tablica[i] = false;
+                Console.WriteLine(tablica[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
