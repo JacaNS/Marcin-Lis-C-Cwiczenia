e9a6bb986457ed7791669953f5f777d1c250cd62
JacaNS <komurkiewiczjacek@gmail.com>
2018-04-16 09:58:44 +0200

    Cwiczenie 12.6

---
+++ Cwiczenia/Cwiczenie 12.6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_12._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[10];
+            int i = 0;
+            while(++i < tab.Length)
+            {
+                tab[i] = i;
+                Console.WriteLine("Tablica [{0}] = {1}", i, tab[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
