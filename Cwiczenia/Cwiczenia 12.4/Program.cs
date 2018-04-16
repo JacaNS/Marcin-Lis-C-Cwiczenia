15a84933f71484a8b9bdec9682b244a111003027
JacaNS <komurkiewiczjacek@gmail.com>
2018-04-16 09:20:46 +0200

    Cwiczenie 12.4

---
+++ Cwiczenia/Cwiczenia 12.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenia_12._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[100];
+            for(int i = 0; i < tab.Length; i++)
+            {
+                if (i % 10 == 0)
+                    tab[i] = 1;
+                Console.WriteLine(tab[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
