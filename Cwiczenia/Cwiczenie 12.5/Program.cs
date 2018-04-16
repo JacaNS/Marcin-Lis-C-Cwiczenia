f913d65dc945022a91ea43c37e4c580c1e9ca485
JacaNS <komurkiewiczjacek@gmail.com>
2018-04-16 09:42:12 +0200

    Cwiczenie 12.5

---
+++ Cwiczenia/Cwiczenie 12.5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_12._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] tablica = new char[26];
+            char k = 'a';
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                tablica[i] = k++;
+                Console.WriteLine("Tablica[" + i + "] = " + tablica[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
